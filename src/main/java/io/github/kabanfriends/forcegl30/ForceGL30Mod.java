337f505297c108817f1f3e1f3d0456fe5cedb8c3
MATYJAGUZ075 <155926881+MATYJAGUZ075@users.noreply.github.com>
2026-07-02 14:25:02 -0300

    Add files via upload

---
+++ src/main/java/io/github/kabanfriends/forcegl30/ForceGL30Mod.java
@@ -0,0 +1,11 @@
+package io.github.kabanfriends.forcegl30;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ForceGL30Mod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        ForceGL30.LOGGER.info("ForceGL3.0-Remapped loaded.");
+    }
+}
