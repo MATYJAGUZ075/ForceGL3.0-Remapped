ae62f29d9d24063d09e05dbfd71bb29b5e7f646f
MATYJAGUZ075 <155926881+MATYJAGUZ075@users.noreply.github.com>
2026-07-02 19:27:11 -0300

    Update ForceGL30.java

--- src(26.1.2)/main/java/io/github/kabanfriends/forcegl30/ForceGL30.java
+++ src(26.1.2)/main/java/io/github/kabanfriends/forcegl30/ForceGL30.java
@@ -3,48 +3,51 @@
 // (powered by Fernflower decompiler)
 //
 
-package io.github.kabanfriends.forcegl30.mixin;
+package io.github.kabanfriends.forcegl30;
 
-import com.mojang.blaze3d.opengl.GlBackend;
-import io.github.kabanfriends.forcegl30.ForceGL30;
+import com.google.common.collect.ImmutableMap;
 import io.github.kabanfriends.forcegl30.override.HintOverride;
 import io.github.kabanfriends.forcegl30.override.OverrideType;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.List;
 import org.lwjgl.glfw.GLFW;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-@Mixin({GlBackend.class})
-public class GL30Mixin {
-    public GL30Mixin() {
+public class ForceGL30 {
+    public static final Logger LOGGER = LoggerFactory.getLogger("ForceGL30");
+    public static final ImmutableMap<Integer, HintOverride> GLFW_OVERRIDE_VALUES;
+    public static final ImmutableMap<Integer, String> GLFW_HINT_NAMES;
+    private static final int[] GLFW_HINT_CODES = new int[]{131073, 131074, 131075, 131076, 131077, 131078, 131079, 131080, 131081, 131082, 131083, 131084, 135169, 135170, 135171, 135172, 135173, 135174, 135175, 135176, 135177, 135178, 135179, 135180, 135181, 135182, 135183, 135184, 139265, 139266, 139267, 139268, 139269, 139270, 139271, 139272, 139273, 139274, 139275, 139276, 143361, 143362, 143363, 147457, 147458};
+
+    public ForceGL30() {
     }
 
-    @Redirect(
-        method = {"setWindowHints"},
-        at = @At(
-    value = "INVOKE",
-    target = "Lorg/lwjgl/glfw/GLFW;glfwWindowHint(II)V",
-    remap = false
-)
-    )
-    private void windowHintOverride(int hint, int value) {
-        System.out.println("FORCEGL30 MIXIN FUNCIONANDO");
-        if (ForceGL30.GLFW_OVERRIDE_VALUES.containsKey(hint)) {
-            int original = value;
-            String hintName = "Unknown";
-            if (ForceGL30.GLFW_HINT_NAMES.containsKey(hint)) {
-                hintName = (String)ForceGL30.GLFW_HINT_NAMES.get(hint);
-            }
+    static {
+        ImmutableMap.Builder<Integer, HintOverride> overrideBuilder = ImmutableMap.builder();
+        overrideBuilder.put(139266, new HintOverride(OverrideType.SET_VALUE, 3));
+        overrideBuilder.put(139267, new HintOverride(OverrideType.SET_VALUE, 0));
+        overrideBuilder.put(139272, new HintOverride(OverrideType.SET_VALUE, 0));
+        overrideBuilder.put(139270, new HintOverride(OverrideType.SET_VALUE, 1));
+        GLFW_OVERRIDE_VALUES = overrideBuilder.build();
+        ImmutableMap.Builder<Integer, String> nameBuilder = ImmutableMap.builder();
+        List<Integer> codes = Arrays.stream(GLFW_HINT_CODES).boxed().toList();
 
-            HintOverride override = (HintOverride)ForceGL30.GLFW_OVERRIDE_VALUES.get(hint);
-            OverrideType type = override.getOverrideType();
-            value = override.getValue();
-            ForceGL30.LOGGER.info("Overriding {}: {} -> {}", new Object[]{hintName, original, type == OverrideType.DO_NOT_SET ? "None" : value});
-            if (type == OverrideType.DO_NOT_SET) {
-                return;
+        for(Field field : GLFW.class.getDeclaredFields()) {
+            if (!field.getName().equals("GLFW_OPENGL_DEBUG_CONTEXT") && field.getType() == Integer.TYPE && Modifier.isPublic(field.getModifiers()) && Modifier.isFinal(field.getModifiers())) {
+                try {
+                    int code = (Integer)field.get((Object)null);
+                    if (codes.contains(code)) {
+                        nameBuilder.put(code, field.getName());
+                    }
+                } catch (IllegalAccessException var8) {
+                    LOGGER.error("Failed to get field value for GLFW.{}", field.getName());
+                }
             }
         }
 
-        GLFW.glfwWindowHint(hint, value);
+        GLFW_HINT_NAMES = nameBuilder.build();
     }
 }
