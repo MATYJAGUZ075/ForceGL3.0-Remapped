75ab01dae62c4c42f1a635fe7eb94eb8023e26d2
MATYJAGUZ075 <155926881+MATYJAGUZ075@users.noreply.github.com>
2026-07-02 19:33:35 -0300

    Update OverrideType.java

--- src(26.1.2)/main/java/io/github/kabanfriends/forcegl30/override/OverrideType.java
+++ src(26.1.2)/main/java/io/github/kabanfriends/forcegl30/override/OverrideType.java
@@ -1,6 +1,14 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
 package io.github.kabanfriends.forcegl30.override;
 
 public enum OverrideType {
     SET_VALUE,
-    DO_NOT_SET
+    DO_NOT_SET;
+
+    private OverrideType() {
+    }
 }
