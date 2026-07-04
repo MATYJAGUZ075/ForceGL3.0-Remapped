7f33da3867bb2908f866bc005db3fc2d6a0431fa
MATYJAGUZ075 <155926881+MATYJAGUZ075@users.noreply.github.com>
2026-07-04 16:42:56 -0300

    Create GLFeature.java

---
+++ src/client/java/io/github/kabanfriends/forcegl30/compat/GLFeature.java
@@ -0,0 +1,23 @@
+package io.github.kabanfriends.forcegl30.compat;
+
+public enum GLFeature {
+
+    VERTEX_ATTRIB_BINDING,
+    DIRECT_STATE_ACCESS,
+    BUFFER_STORAGE,
+    BASE_INSTANCE,
+    DRAW_INDIRECT,
+    MULTI_DRAW_INDIRECT,
+    SHADER_DRAW_PARAMETERS,
+    CLIP_CONTROL,
+
+    // Texture Buffer
+    TEX_BUFFER,
+    ARB_TEX_BUFFER,
+    TEX_BUFFER_RANGE,
+
+    // Uniform Buffer Object
+    UNIFORM_BUFFER,
+    ARB_UNIFORM_BUFFER
+
+}
