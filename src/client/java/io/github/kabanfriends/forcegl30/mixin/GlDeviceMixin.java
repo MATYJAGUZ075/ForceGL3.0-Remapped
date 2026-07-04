7f8a0bd4548fa7f8578927d49b09d36ea73e26a7
MATYJAGUZ075 <155926881+MATYJAGUZ075@users.noreply.github.com>
2026-07-04 16:35:38 -0300

    Update GlDeviceMixin.java

--- src/client/java/io/github/kabanfriends/forcegl30/mixin/GlDeviceMixin.java
+++ src/client/java/io/github/kabanfriends/forcegl30/mixin/GlDeviceMixin.java
@@ -2,7 +2,10 @@ package io.github.kabanfriends.forcegl30.mixin;
 
 import com.mojang.blaze3d.shaders.GpuDebugOptions;
 import com.mojang.blaze3d.shaders.ShaderSource;
+import io.github.kabanfriends.forcegl30.ForceGL30;
+import io.github.kabanfriends.forcegl30.compat.CompatibilityManager;
 import org.lwjgl.opengl.GL;
+import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GLCapabilities;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -27,20 +30,39 @@ public class GlDeviceMixin {
             GpuDebugOptions debugOptions,
             CallbackInfo ci
     ) {
+
         GLCapabilities caps = GL.getCapabilities();
 
-        System.out.println("========== FORCEGL30 ==========");
-        System.out.println("GL_ARB_vertex_attrib_binding = " + caps.GL_ARB_vertex_attrib_binding);
-        System.out.println("GL_ARB_direct_state_access   = " + caps.GL_ARB_direct_state_access);
-        System.out.println("GL_ARB_buffer_storage        = " + caps.GL_ARB_buffer_storage);
-        System.out.println("GL_ARB_base_instance         = " + caps.GL_ARB_base_instance);
-        System.out.println("GL_ARB_draw_indirect         = " + caps.GL_ARB_draw_indirect);
-        System.out.println("GL_ARB_instanced_arrays      = " + caps.GL_ARB_instanced_arrays);
-        System.out.println("GL_ARB_draw_elements_base_vertex = " + caps.GL_ARB_draw_elements_base_vertex);
-        System.out.println("GL_ARB_draw_instanced       = " + caps.GL_ARB_draw_instanced);
-        System.out.println("GL_ARB_multi_draw_indirect   = " + caps.GL_ARB_multi_draw_indirect);
-        System.out.println("GL_ARB_shader_draw_parameters= " + caps.GL_ARB_shader_draw_parameters);
-        System.out.println("GL_ARB_clip_control          = " + caps.GL_ARB_clip_control);
-        System.out.println("================================");
+        CompatibilityManager.init(caps);
+
+        ForceGL30.LOGGER.info("========== ForceGL30 ==========");
+        ForceGL30.LOGGER.info("GL_VERSION  = {}", GL11.glGetString(GL11.GL_VERSION));
+        ForceGL30.LOGGER.info("GL_VENDOR   = {}", GL11.glGetString(GL11.GL_VENDOR));
+        ForceGL30.LOGGER.info("GL_RENDERER = {}", GL11.glGetString(GL11.GL_RENDERER));
+
+        ForceGL30.LOGGER.info("--------------------------------");
+
+        ForceGL30.LOGGER.info("OpenGL30 = {}", caps.OpenGL30);
+        ForceGL30.LOGGER.info("OpenGL31 = {}", caps.OpenGL31);
+        ForceGL30.LOGGER.info("OpenGL32 = {}", caps.OpenGL32);
+        ForceGL30.LOGGER.info("OpenGL33 = {}", caps.OpenGL33);
+
+        ForceGL30.LOGGER.info("--------------------------------");
+
+        ForceGL30.LOGGER.info("GL_ARB_texture_buffer_object      = {}", caps.GL_ARB_texture_buffer_object);
+        ForceGL30.LOGGER.info("GL_ARB_uniform_buffer_object      = {}", caps.GL_ARB_uniform_buffer_object);
+        ForceGL30.LOGGER.info("GL_ARB_vertex_attrib_binding      = {}", caps.GL_ARB_vertex_attrib_binding);
+        ForceGL30.LOGGER.info("GL_ARB_direct_state_access        = {}", caps.GL_ARB_direct_state_access);
+        ForceGL30.LOGGER.info("GL_ARB_buffer_storage             = {}", caps.GL_ARB_buffer_storage);
+        ForceGL30.LOGGER.info("GL_ARB_base_instance              = {}", caps.GL_ARB_base_instance);
+        ForceGL30.LOGGER.info("GL_ARB_draw_indirect              = {}", caps.GL_ARB_draw_indirect);
+        ForceGL30.LOGGER.info("GL_ARB_instanced_arrays           = {}", caps.GL_ARB_instanced_arrays);
+        ForceGL30.LOGGER.info("GL_ARB_draw_elements_base_vertex  = {}", caps.GL_ARB_draw_elements_base_vertex);
+        ForceGL30.LOGGER.info("GL_ARB_draw_instanced             = {}", caps.GL_ARB_draw_instanced);
+        ForceGL30.LOGGER.info("GL_ARB_multi_draw_indirect        = {}", caps.GL_ARB_multi_draw_indirect);
+        ForceGL30.LOGGER.info("GL_ARB_shader_draw_parameters     = {}", caps.GL_ARB_shader_draw_parameters);
+        ForceGL30.LOGGER.info("GL_ARB_clip_control               = {}", caps.GL_ARB_clip_control);
+
+        ForceGL30.LOGGER.info("================================");
     }
 }
